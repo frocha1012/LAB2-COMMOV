b43d888a5c38a2bd5b3bd4b5ea4566497f78e2bd
Fernando Rocha <frocha2910@gmail.com>
2024-04-04 14:23:35 +0100

    Remove all repository content

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("androidx.navigation.safeargs") version "2.7.7" apply false
-}
